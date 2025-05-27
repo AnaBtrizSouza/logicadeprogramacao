b70986ecff8fffc4c35e7c78fd7a614e28b8342f
Ana <ana.souzabeatriz2009@gmail.com>
2025-05-27 11:30:11 -0300

    funcoes2705

---
+++ Funcoes/LogproDesafio2.js.js
@@ -0,0 +1,76 @@
+// desafio 1 
+/*function login(){ 
+	let mensagem = prompt("Digite o login: ") 
+	let senha = prompt("Digite a senha: ") 
+    	if(mensagem == "Ana" && senha == 230809){ 
+    	console.log("Login efetuado") 
+    	} 
+    	else{ 
+        	console.log("Usuario ou senha icorreto, tente novamente") 
+    	} 
+} 
+login()*/ 
+ 
+//desafio 2  
+let mensagem = prompt("Digite o login: ") 
+let senha = prompt("Digite a senha: ") 
+  
+let usuario1 = "Ana1" 
+let senha1 = "11" 
+  
+let usuario2 = "Ana2" 
+let senha2 = "22" 
+  
+let usuario3 = "Ana3" 
+let senha3 = "33" 
+  
+let usuario4 = "Ana4" 
+let senha4 = "44" 
+  
+let usuario5 = "Ana5" 
+let senha5 = "55" 
+  
+let usuario6 = "Ana6" 
+let senha6 = "66" 
+  
+let usuario7 = "Ana7" 
+let senha7 = "77" 
+  
+let usuario8 = "Ana8" 
+let senha8 = "88" 
+  
+let usuario9 = "Ana9" 
+let senha9 = "99" 
+  
+let usuario10 = "Ana10" 
+let senha10 = "1010" 
+  
+function usuarios(){ 
+  	if(mensagem == usuario1 && senha == senha1){ 
+    	console.log("Login efetuado") 
+    	}else if (mensagem == usuario2 && senha == senha2){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem == usuario3 && senha == senha3){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem == usuario4 && senha == senha4){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem == usuario5 && senha == senha5){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem== usuario6 && senha == senha6){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem == usuario7 && senha == senha7){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem == usuario8 && senha == senha8){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem == usuario9 && senha == senha9){ 
+         	console.log("Login efetuado") 
+    	}else if (mensagem == usuario10 && senha == senha10){ 
+         	console.log("Login efetuado") 
+    	} 
+    	else{ 
+        	console.log("Usuario ou senha icorreto, tente novamente") 
+    	} 
+} 
+usuarios() 
+ 
+
