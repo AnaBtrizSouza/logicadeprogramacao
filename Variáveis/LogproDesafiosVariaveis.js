e9e6c9735e7d6ffb7b2809a27ced743c12933b77
Ana <ana.souzabeatriz2009@gmail.com>
2025-06-03 10:48:44 -0300

    Desafios Variaveis

---
+++ Variáveis/LogproDesafiosVariaveis.js
@@ -0,0 +1,15 @@
+let nome = prompt("Digite seu nome: ")
+let idade = Number(prompt("Digite sua idade: "))
+let email = prompt("Digite seu e-mail: ")
+let telefone = Number(prompt("Digite seu telefone: "))
+console.log("O seu nome é", nome)
+console.log("Você tem", idade, "anos")
+console.log("Pertence a você o e-mail:", email)
+console.log("e seu número de telefone é", telefone)
+console.log("Obrigada pelas informações, agora podemos continuar...")
+
+let data = prompt("Qual é a seu ano de nascimento?")
+let ano = 2025
+let resultado = ano - data
+console.log(resultado)
+console.log("Você tem", resultado, "anos de idade")
