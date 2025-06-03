b420ab9880b07ef3e929cd9a9f72194ea8954892
Ana <ana.souzabeatriz2009@gmail.com>
2025-06-03 10:52:48 -0300

    Exercicios IF ELSE

--- Condicionais/IFELSE/LogproDesafio_IFELSE.js
+++ Condicionais/IFELSE/LogproDesafio_IFELSE.js
@@ -1,177 +1,183 @@
 let respostaCorreta = 0
 let respostaIncorreta = 0
 
-//Pergunta1
-console.log(&quot;Quem é a rainha decapitada durante a revolução francesa? &quot;)
-console.log(&quot;A - Maria Antonieta &quot;)
-console.log(&quot;B - Cleopatra&quot;)
-console.log(&quot;C - Elizabeth&quot;)
-console.log(&quot;D - Maria II&quot;)
-let pergunta1 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta1 == &quot;A&quot;|| pergunta1 ==&quot;Maria Antonieta&quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+//Pergunta1 
+console.log("Quem foi a primeira princesa da diseny? ")
+console.log("A - Branca de neve ")
+console.log("B - Cinderela")
+console.log("C - Bela")
+console.log("D - Aurora")
+let pergunta1 = prompt("Digite a alternativa correta: ")
+if(pergunta1 == "A"|| pergunta1 =="Branca de neve"){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
-//Pergunta2
-console.log(&quot;\n Qual foi o primeiro presidente do Brasil após o fim da ditadura militar? &quot;)
-console.log(&quot;A - Sarney &quot;)
-console.log(&quot;B - Vargas&quot;)
-console.log(&quot;C - Brizola&quot;)
-console.log(&quot;D - Deodoro&quot;)
-let pergunta2 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta2 == &quot;A&quot;|| pergunta2 == &quot;Sarney &quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+
+
+//Pergunta2 
+console.log("\n Qual o nome da rainha dos Trolls no filme Trolls ")
+console.log("A - Poppy ")
+console.log("B - DJ")
+console.log("C - Nina")
+console.log("D - Guy Diamante")
+let pergunta2 = prompt("Digite a alternativa correta: ")
+if(pergunta2 == "A"|| pergunta2 == "Poppy "){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
-//Pergunta3
-console.log(&quot;\n Quem foi o primeiro imperador do Brasil ? &quot;)
-console.log(&quot;A - Dom João &quot;)
-console.log(&quot;B - Tiradentes&quot;)
-console.log(&quot;C - Dom Pedro I&quot;)
-console.log(&quot;D - Dom Pedro II&quot;)
-let pergunta3 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta3 == &quot;C&quot;|| pergunta3 ==&quot;Dom Pedro I&quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+
+//Pergunta3 
+console.log("\n Quantos filmes tem a saga Harry Potter? ? ")
+console.log("A - 9 ")
+console.log("B - 7")
+console.log("C - 8")
+console.log("D - 6")
+let pergunta3 = prompt("Digite a alternativa correta: ")
+if(pergunta3 == "C"|| pergunta3 =="8"){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
 //pergunta4
-console.log(&quot;\n Onde a humanidade aprendeu a agricultura? &quot;)
-console.log(&quot;A - Espanha &quot;)
-console.log(&quot;B - No mar&quot;)
-console.log(&quot;C - Crescente fértil&quot;)
-console.log(&quot;D - Grécia&quot;)
-let pergunta4 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta4 == &quot;C&quot;|| pergunta4 ==&quot;Crescente fértil &quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+console.log("\n Onde foi gravada a cena do trem de Hogwats no filme Harry Potter? ")
+console.log("A - Espanha ")
+console.log("B - Noruega")
+console.log("C - Escócia")
+console.log("D - Grécia")
+let pergunta4 = prompt("Digite a alternativa correta: ")
+if(pergunta4 == "C"|| pergunta4 =="Escócia "){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
+
 //Pergunta5
-console.log(&quot;\n Quem foi o ditador da Alemanha na Segunda Guerra? &quot;)
-console.log(&quot;A - Mussolini &quot;)
-console.log(&quot;B - Hitler&quot;)
-console.log(&quot;C - Stalin&quot;)
-console.log(&quot;D - Lênin&quot;)
-let pergunta5 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta5 == &quot;B&quot;|| pergunta5 ==&quot;Hitler &quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+console.log("\n Quem foi o criador da disney? ")
+console.log("A - Martin Scorsese ")
+console.log("B - Walt Disney")
+console.log("C - Spike Lee")
+console.log("D - Tim Burton")
+let pergunta5 = prompt("Digite a alternativa correta: ")
+if(pergunta5 == "B"|| pergunta5 =="Hitler "){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
 //Pergunta6
-console.log(&quot;\n Quem pintou a Monalisa &quot;)
-console.log(&quot;A - DaVinci &quot;)
-console.log(&quot;B - Rafael&quot;)
-console.log(&quot;C - Caravaggio&quot;)
-console.log(&quot;D - Picasso&quot;)
-let pergunta6 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta6 == &quot;A&quot;|| pergunta6 ==&quot;DaVinci &quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+console.log("\n em que foi inspirado Pocahontas da disney ")
+console.log("A - Matoaka ")
+console.log("B - Freya")
+console.log("C - Dahlia")
+console.log("D - Willow")
+let pergunta6 = prompt("Digite a alternativa correta: ")
+if(pergunta6 == "A"|| pergunta6 =="Matoaka "){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
 //Pergunta7
-console.log(&quot;\n Que país colonizou o Brasil? &quot;)
-console.log(&quot;A - Espanha &quot;)
-console.log(&quot;B - Portugal&quot;)
-console.log(&quot;C - França&quot;)
-console.log(&quot;D - Itália&quot;)
-let pergunta7 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta7 == &quot;B&quot;|| pergunta7 ==&quot;Portugal &quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+console.log("\n Que país se passa Pocahontas? ")
+console.log("A - Espanha ")
+console.log("B - Estados Unidos")
+console.log("C - França")
+console.log("D - Itália")
+let pergunta7 = prompt("Digite a alternativa correta: ")
+if(pergunta7 == "B"|| pergunta7 =="Estados Unidos "){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
+
 //Pergunta8
-console.log(&quot;\n Que povo criou os Jogos Olímpicos? &quot;)
-console.log(&quot;A - Gregos &quot;)
-console.log(&quot;B - Romanos&quot;)
-console.log(&quot;C - Árabes&quot;)
-console.log(&quot;D - Chineses&quot;)
-let pergunta8 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta8 == &quot;A&quot;|| pergunta8 ==&quot;Gregos &quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+console.log("\n Que povo é representado no filme Lilo e Stitch? ")
+console.log("A - Havainos ")
+console.log("B - Romanos")
+console.log("C - Árabes")
+console.log("D - Chineses")
+let pergunta8 = prompt("Digite a alternativa correta: ")
+if(pergunta8 == "A"|| pergunta8 =="Gregos "){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
 //Pergunta9
-console.log(&quot;\n Que país lançou a primeira bomba atômica? &quot;)
-console.log(&quot;A - Estados Unidos &quot;)
-console.log(&quot;B - Japão&quot;)
-console.log(&quot;C - Italia&quot;)
-console.log(&quot;D - Alemanha&quot;)
-let pergunta9 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta9 == &quot;A&quot;|| pergunta9 ==&quot;Estados Unidos&quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+console.log("\n Que país lançou a primeira animação? ")
+console.log("A - França ")
+console.log("B - Japão")
+console.log("C - Italia")
+console.log("D - Alemanha")
+let pergunta9 = prompt("Digite a alternativa correta: ")
+if(pergunta9 == "A"|| pergunta9 =="Estados Unidos"){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
+
 //Pergunta10
-console.log(&quot;\n Que dia é comemorado a Independência do Brasil? &quot;)
-console.log(&quot;A - 25/12 &quot;)
-console.log(&quot;B - 1/4&quot;)
-console.log(&quot;C - 2/10&quot;)
-console.log(&quot;D - 7/9&quot;)
-let pergunta10 = prompt(&quot;Digite a alternativa correta: &quot;)
-if(pergunta10 == &quot;D&quot;|| pergunta10 ==&quot;7/9&quot;){
-console.log(&quot;Alternativa correta!&quot;)
-respostaCorreta ++
+console.log("\n Que dia é comemorado o dia internacional da animação? ")
+console.log("A - 25/12 ")
+console.log("B - 1/4")
+console.log("C - 2/10")
+console.log("D - 28/10")
+let pergunta10 = prompt("Digite a alternativa correta: ")
+if(pergunta10 == "D"|| pergunta10 =="28/10"){
+    console.log("Alternativa correta!")
+    respostaCorreta ++
 }
 else{
-console.log(&quot;Alternativa errada! &quot;)
+    console.log("Alternativa errada! ")
 
 respostaIncorreta ++
 }
 
 //Exibir Resultado
-console.log(&quot;\n Resumo final: &quot;)
-console.log(&quot; Respostas Corretas : &quot; + respostaCorreta)
-console.log(&quot; Respostas incorretas : &quot; + respostaIncorreta)
+console.log("\n Resumo final: ")
+console.log(" Respostas Corretas : " + respostaCorreta)
+console.log(" Respostas incorretas : " + respostaIncorreta)
---
+++ Condicionais/IFELSE/exerciciosIFELSE.js
@@ -0,0 +1,127 @@
+Exercício 1
+let idade = prompt(&quot;digite a sua idade: &quot;)
+if (idade &lt; 18) {
+console.log(&quot;você é menor de idade&quot;)
+}
+else {
+console.log(&quot;você é maior de idade&quot;)
+}
+Exercício 2
+let horario = prompt(&quot;qual o horario?&quot;)
+if(horario &lt;= 12){
+console.log(&quot;bom dia&quot;)
+}
+else if(horario &lt;= 18){
+console.log(&quot;boa tarde&quot;)
+}
+else {
+console.log(&quot;boa noite&quot;)
+}
+Exercício 3
+let idade = prompt(&quot;digite a sua idade: &quot;)
+if (idade &lt; 16) {
+console.log(&quot;acesso negado&quot;)
+}
+else {
+console.log(&quot;acesso permitido&quot;)
+}
+Exercício 4
+let dados1 = 10
+let dados2 = 50
+if (dados1 &lt; dados2){
+
+console.log(&quot;dados2 possui maior valor&quot;)
+}
+else (dados1 &gt; dados2){
+console.log(&quot;dados1 possui maior valor&quot;)
+}
+Exercício 5
+let clima = 22
+if (clima &gt;= 26){
+console.log(&quot;quente&quot;)
+}
+else if(clima &gt;= 20){
+console.log(&quot;termico&quot;)
+}
+else {
+console.log(&quot;frio&quot;)
+}
+Exercício 6
+let palavra = prompt(&quot;Qual a palavra?&quot;)
+if (palavra == &quot;Felicidade&quot;){
+console.log(&quot;palavra correta&quot;)
+}
+else {
+console.log(&quot;palavra incorreta&quot;)
+}
+Exercício 7
+let quantidade = prompt(&quot;qual a quantidade de moedas?&quot;)
+if (quantidade &lt;= 10){
+console.log(&quot;poucas moedas&quot;)
+}
+else if(quantidade &lt;= 20){
+console.log(&quot;quantidade media&quot;)
+
+}
+else {
+console.log(&quot;poucas moedas&quot;)
+}
+Exercício 8
+let temperatura = 14
+if (temperatura &gt;= 30){
+console.log(&quot;muito quente&quot;)
+}
+else if (temperatura &gt;= 20){
+console.log(&quot;calor&quot;)
+}
+else {
+console.log(&quot;frio&quot;)
+}
+Exercício 9
+*let numero = -9
+if (numero &lt; 0){
+console.log(&quot;negativo&quot;)
+}
+else {
+console.log(&quot;positivo&quot;)
+}
+Exercício 10
+let nota = 7
+if(nota &gt;= 7){
+console.log(&quot;aprovado&quot;)
+}
+else{
+console.log(&quot;reprovado&quot;)
+}
+
+Exercício 11
+let animal1 = &quot;gato&quot;
+let animal2 = &quot;cachorro&quot;
+let animal3 = &quot;passáro&quot;
+if (animal1 === &quot;gato&quot; &amp;&amp; animal2 === &quot;cachorro&quot; &amp;&amp; animal3 === &quot;passáro&quot; ){
+console.log(&quot;animal listado&quot;)
+}
+else{
+console.log(&quot;animal não listado&quot;)
+}
+Exercício 12
+let diadasemana = prompt(&quot;qual o dia da semana?&quot;)
+if (diadasemana === &quot;sabado&quot; || &quot;domingo&quot;){
+console.log(&quot;fim de semana&quot;)
+}
+else {
+console.log(&quot;dia util&quot;)
+}
+Exercício 13
+let tempo = prompt(&quot;você possui quantos anos de experiencia?&quot;)
+if (tempo &gt;= 2 ){
+console.log(&quot;experiente&quot;)
+}
+else {
+console.log(&quot;inexperiente&quot;)
+}
+Exercício 14
+let numero = 6
+if (numero &gt;= 10 &amp;&amp; numero &lt;= 20){
+console.log(&quot;dentro do intervalo&quot;)
+}
