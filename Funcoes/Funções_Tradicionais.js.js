8e1cbd120d605565d83e035f1cc4de87fe3a922b
Ana <ana.souzabeatriz2009@gmail.com>
2025-06-03 08:59:23 -0300

    Funções Tradicionais JS

---
+++ Funcoes/Funções_Tradicionais.js.js
@@ -0,0 +1,137 @@
+// 1
+/*function apresentacao() {
+  console.log('Olá, Mundo!');
+}
+apresentacao();*/
+
+//2
+/*function mostrarNome(){
+    let nome = prompt("Qual o seu nome completo? ");
+    console.log("Seu nome é " + nome);
+}
+mostrarNome();*/
+
+//3
+/*function mostrarIdade(){
+    let idade = prompt("Qual é a sua idade? ");
+    console.log("Você tem" , idade , "anos");
+}
+mostrarIdade();*/
+
+//4
+/*function somarDoisNumeros(){
+    let numero1 = Number(prompt("Escolha um número "));
+    let numero2 = Number(prompt("Escolha outro numero "));
+    console.log("A soma dos números é", numero1 + numero2)
+}
+somarDoisNumeros();*/
+
+//5
+/*function mostrarMensagem(){
+    console.log("O que você faz hoje pode melhorar todos os amanhãs.");
+}
+mostrarMensagem()*/
+
+//6
+/*function mostrarData(){
+    let data = prompt("Qual é a data de hoje? ");
+    console.log("A data de hoje é" , data);
+}
+mostrarData()*/
+
+//7
+/*function mostrarCidade(){
+    let cidade = prompt("Escolha uma cidade ");
+    console.log("A cidade escolhida foi", cidade );
+}
+mostrarCidade()*/
+
+//8
+/*function saudacao(){
+    let nome = prompt("Qual o seu nome? ");
+    console.log("Bem-vindo (a) " + nome);
+}
+saudacao();*/
+
+// 9
+/*function subtracao(){
+    let numero1 = prompt("Escolha um número ");
+    let numero2 = prompt("Escolha outro numero ");
+    console.log("A subtração desses numeros é igual a ", numero1 - numero2);
+}
+subtracao();*/
+
+//10
+/*function mostrarAnimalFavorito(){
+    let animal = prompt("Qual é o seu animal favorito? ");
+    console.log("Seu animal favorito é", animal);
+}
+mostrarAnimalFavorito();*/
+
+//11
+/*function mensagem(){
+    console.log ("Estou aprendendo JavaScript");
+}
+mensagem();*/
+
+//12
+/*function triplo(){
+    let numero = prompt("Escolha um número ")
+    console.log("O triplo desse número é", numero * 3)
+}
+triplo()*/
+
+//13
+/*function informacoes(){
+    let nome = prompt("Qual é o seu nome? ")
+    let idade = prompt("Quatos anos você tem? ")
+    console.log("Você se chama", nome , "e tem", 16, "anos de idade")
+}
+informacoes()*/
+
+//14
+/*function numeros(){
+    console.log( "1 2 3 4 5 6 7 8 9 10")
+}
+numeros()*/
+
+//15
+/*function exibir(){
+    console.log("Aula de funções iniciada!")
+}
+exibir()*/
+
+//16
+/*function numero(){
+ let numero = prompt("Escolha um número ")
+    console.log("Você digitou o número", numero)
+}
+numero()*/
+
+//17
+/*function curso(){
+    let curso = prompt("Qual curso você faz? ")
+    console.log("O curso é", curso)
+}
+curso()*/
+
+//18
+/*function chamada(){
+    let chamada = prompt("Insira o seu nome: ")
+    console.log(chamada, "está na chamada")
+}
+chamada()*/
+
+//19
+/*function cor(){
+    let cor = prompt("Qual é a sua cor favorita? ")
+    console.log("Sua cor favorita é", cor)
+}
+cor()*/
+
+//20
+/*function país(){
+    let pais = prompt("Em que país você mora? ")
+    console.log("Você mora no", pais)
+}
+país()*/
